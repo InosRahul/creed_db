1f0898d10cc0dfc9a8e8950dce242d6defdaf529
Rahul Soni <rahulsoni2225@gmail.com>
2021-08-27 11:28:43 +0530

    fixed: footer component, with addon link button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,21 @@ import React from "react";
 const Footer = () => {
    
     return(
-    
-   
-
-    <div className="footer">
-    
+        <div className="footer">
+        <div className="btns">
+                <button className="btn">
+                    <a 
+                    href="https://addons.mozilla.org/en-US/firefox/addon/creeds-thoughts/"
+                    target="_blank"
+                    rel="noreferrer"
+                    title="firefox addon"
+                    >
+                    Firefox Addon
+                    </a>
+                </button>
+        </div>
         <h3>Made with 💛 by an Office Fan.</h3>
-        
-    </div>
+        </div>
 
     )
 }
